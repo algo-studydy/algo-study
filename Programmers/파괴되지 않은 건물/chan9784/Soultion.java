3023a851c48717213da6d09ed44daef2a760bb7c
chanhong9784 <chanhong9784@naver.com>
2024-10-15 07:37:14 +0900

    [Programmers] 파괴되지 않은 건물

--- Programmers/파괴되지 않은 건물/chan9784/Soultion.java
+++ Programmers/파괴되지 않은 건물/chan9784/Soultion.java
@@ -1,6 +1,26 @@
 class Solution {
     public int solution(int[][] board, int[][] skill) {
         int answer = 0;
+        for(int i = 0; i < skill.length; i++) {
+            for(int j = skill[i][1]; j <= skill[i][3]; j++) {
+                for(int k = skill[i][2]; k <= skill[i][4]; k++) {
+                    if(skill[i][0] == 1) {
+                        board[j][k] -= skill[i][5];
+                    } else {
+                        board[j][k] += skill[i][5];
+                    }
+                }
+            }
+        }
+        
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(board[i][j] > 0) {
+                    answer++;
+                }
+            }
+        }
+        
         return answer;
     }
 }
