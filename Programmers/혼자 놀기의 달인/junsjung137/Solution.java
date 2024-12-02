226ccaf3555edce2d4804748b72b9e33134f80f4
Jung Jun Su <junsjung137@gmail.com>
2024-12-03 07:32:35 +0900

    Update Solution.java

--- Programmers/혼자 놀기의 달인/junsjung137/Solution.java
+++ Programmers/혼자 놀기의 달인/junsjung137/Solution.java
@@ -2,34 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] cards) {
-        int maxScore = 0;
+        boolean[] visited = new boolean[cards.length];
+        ArrayList<Integer> group = new ArrayList<>();
 
-        for (int start = 0; start < cards.length; start++) {
-            boolean[] visited = new boolean[cards.length];
-            ArrayList<Integer> group = new ArrayList<>();
+        for (int i = 0; i < cards.length; i++) {
+            if (!visited[i]) {
+                int groupCount = 0;
+                int current = i;
 
-            for (int i = 0; i < cards.length; i++) {
-                if (!visited[i]) {
-                    int groupCount = 0;
-                    int current = i;
-
-                    while (!visited[current]) {
-                        visited[current] = true;
-                        groupCount++;
-                        current = cards[current] - 1;
-                    }
-                    group.add(groupCount);
+                while (!visited[current]) {
+                    visited[current] = true;
+                    groupCount++;
+                    current = cards[current] - 1;
                 }
-            }
-
-            Collections.sort(group, Collections.reverseOrder());
-
-            if (group.size() >= 2) {
-                int score = group.get(0) * group.get(1);
-                maxScore = Math.max(maxScore, score);
+                group.add(groupCount);
             }
         }
 
-        return maxScore;
+        Collections.sort(group, Collections.reverseOrder());
+
+        if (group.size() < 2) {
+            return 0;
+        } else {
+            return group.get(0) * group.get(1);
+        }
     }
 }
