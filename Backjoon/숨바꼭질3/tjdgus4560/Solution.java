ced08ef07d0460f66f618780220436216ecfc0d4
HanSeongHyun <tjdgus4560@naver.com>
2024-12-17 20:49:30 +0900

    [Backjoon] 숨바꼭질3

--- Backjoon/숨바꼭질3/tjdgus4560/Solution.java
+++ Backjoon/숨바꼭질3/tjdgus4560/Solution.java
@@ -1,56 +1,59 @@
+package Baekjoon;
+
 import java.util.*;
 
+public class 숨바꼭질3 {
+    static int[] walk;
 
-class Solution {
-    public int solution(int N, int number) {
-        if (N == number) {
-            return 1;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 수빈이의 위치
+        int K = sc.nextInt(); // 동생의 위치
+
+        System.out.println(bfs(N, K));
+        sc.close();
+    }
+
+    private static int bfs(int start, int target) {
+        if (start >= target) {
+            return start - target;
         }
+        // 방문배열 초기화
+        int max = 100000;
+        int[] visited = new int[max + 1];
+        Arrays.fill(visited, -1);
 
-        // 초기화
-        List<Set<Integer>> dp = new ArrayList<>();
-        for (int i = 0; i <= 8; i++) {
-            dp.add(new HashSet<>());
-        }
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = 0; // 시작 위치의 시간은 0
 
-        // 1번 사용했을 때는 N만 가능
-        dp.get(1).add(N);
+        while (!q.isEmpty()) {
+            int cur = q.poll();
 
-        // N을 i번 사용하면서 가능한 조합들
-        for(int i = 2; i<=8 ;i++){
-            // N을 이어붙인 숫자
-            int tmp = 0;
-            for(int j = 0; j<i ;j++){
-                tmp += Math.pow(10, j)*N;
+            if (cur == target) {
+                return visited[cur];
             }
-            dp.get(i).add(tmp);
 
-            // i-1까지 사용가능한 조합들을 이용한 계산 추가
-            for (int j = 1; j < i/2+1; j++) {
-                // N을 총i 번 사용하는 조합을 계산하기 위해 j, i-j dp 의 원소를 이용
-                for (int num1 : dp.get(j)) {
-                    for (int num2 : dp.get(i - j)) {
-                        dp.get(i).add(num1 + num2); // 덧셈
-                        dp.get(i).add(num1 - num2); // 뺄셈
-                        dp.get(i).add(num2 - num1); // 뺄셈
-                        dp.get(i).add(num1 * num2); // 곱셈
-                        if (num2 != 0) {
-                            dp.get(i).add(num1 / num2); // 나눗셈
-                        }
-                        if (num1 != 0) {
-                            dp.get(i).add(num2 / num1); // 나눗셈
-                        }
+            // 순간이동
+            int teleport = cur * 2;
+            if (teleport <= max && visited[teleport] == -1) {
+                visited[teleport] = visited[cur];
+                q.add(teleport);
+            }
 
-                    }
+            // 걷기
+            walk = new int[2];
+            walk[0] = cur - 1;
+            walk[1] = cur + 1;
+            for (int n : walk) {
+                if (n >= 0 && n <= max && visited[n] == -1) {
+                    visited[n] = visited[cur] + 1;
+                    q.add(n);
                 }
             }
-
-            // 목표 숫자가 dp[k]에 있다면 종료
-            if (dp.get(i).contains(number)) {
-                return i;
-            }
         }
 
+        // 이쪽으로 리턴할일은 없음
         return -1;
     }
 }
