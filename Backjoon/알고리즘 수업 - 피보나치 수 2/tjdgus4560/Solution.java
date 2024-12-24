d2eb9f3e301415a5ee2440989c63abadbb98829b
HanSeongHyun <tjdgus4560@naver.com>
2024-12-24 20:21:33 +0900

    [Backjoon] 피보2

--- Backjoon/알고리즘 수업 - 피보나치 수 2/tjdgus4560/Solution.java
+++ Backjoon/알고리즘 수업 - 피보나치 수 2/tjdgus4560/Solution.java
@@ -1,56 +1,22 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int V;
-    static int E;
-    static int R;
-    static ArrayList<Integer>[] adjList;
-    static int[] visit;
-    static boolean[] visited;
-    static int visitedTime = 1;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        V = sc.nextInt();
-        E = sc.nextInt();
-        R = sc.nextInt();
-        adjList = new ArrayList[V + 1];
+        int n = sc.nextInt();
 
-        for (int i = 0; i < V + 1; i++){
-            adjList[i] = new ArrayList<>();
+        int a = 1;
+        int b = 1;
+        int c;
+        for( int i = 3; i <= n; i++ ) {
+            c = (a + b)%1_000_000_007;
+            a = b;
+            b = c;
         }
-        for (int i = 0; i < E; i++){
-            int from = sc.nextInt();
-            int to = sc.nextInt();
-            adjList[from].add(to);
-            adjList[to].add(from);
-        }
-
-        for(int i = 0; i < V + 1; i++){
-            Collections.sort(adjList[i], Collections.reverseOrder());
-        }
-        visit = new int[V + 1];
-        visited = new boolean[V + 1];
-        bfs();
-
-        for(int i = 1; i < V + 1; i++){
-            System.out.println(visit[i]);
-        }
+        System.out.println(b + " " + (n-2));
     }
 
-    private static void bfs() {
-        Queue<Integer> q = new LinkedList<>();
-        visited[R] = true;
-        q.add(R);
-        while(!q.isEmpty()){
-            int v = q.poll();
-            visit[v] = visitedTime++;
-            for(int i : adjList[v]){
-                if(!visited[i]){
-                    visited[i] = true;
-                    q.add(i);
-                }
-            }
-        }
-    }
+
+
+
 }
