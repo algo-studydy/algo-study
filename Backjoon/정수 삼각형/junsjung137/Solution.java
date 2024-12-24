21eb983d4429d61d8de1ef6d8b1ecad6478293e9
jungjunsu <junsjung137@gmail.com>
2024-12-24 18:39:56 +0900

    [Backjoon] 정수 삼각형

---
+++ Backjoon/정수 삼각형/junsjung137/Solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}
