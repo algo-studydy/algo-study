aba52d6a4f0b0616cc4569b09557848ac73ab62e
jungjunsu <junsjung137@gmail.com>
2025-02-04 09:06:42 +0900

    [Backjoon] 수열

---
+++ Backjoon/수열/junsjung137/Solution.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for (int i = 0; i < K; i++) {
+            sum += arr[i];
+        }
+
+        int max = sum;
+        for (int i = K; i < N; i++) {
+            sum += arr[i] - arr[i-K];
+            max = Math.max(max, sum);
+        }
+        System.out.println(max);
+    }
+}
