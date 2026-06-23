42f0ad2777153e7da389803c9672bc375d4ea4b2
hjkim <denny10002@naver.com>
2026-06-23 20:20:43 +0900

    solve: dp1 1-2

---
+++ Codetree/김현준/dp1/ch1/사각형 채우기.java
@@ -0,0 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[] memo;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N+1];
+        if(N <= 2) {
+            System.out.println(N); return;
+        };
+        dp[1] = 1; dp[2] = 2;
+        for(int i=3;i<=N;i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+
+        System.out.println(dp[N]);
+    }
+
+
+}
